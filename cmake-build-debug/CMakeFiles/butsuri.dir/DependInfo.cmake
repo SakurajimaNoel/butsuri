f962bcd27313ceffa5d3b9f708aba5c8af94213d
Sakurajima Noel <amitkoranga2@gmail.com>
2021-05-05 22:58:33 +0530

    CMAKE amends

---
+++ cmake-build-debug/CMakeFiles/butsuri.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nitinsikka/Desktop/butsuri/src/main.cpp" "/Users/nitinsikka/Desktop/butsuri/cmake-build-debug/CMakeFiles/butsuri.dir/src/main.cpp.o"
+  "/Users/nitinsikka/Desktop/butsuri/src/window.cpp" "/Users/nitinsikka/Desktop/butsuri/cmake-build-debug/CMakeFiles/butsuri.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/butsuri.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/butsuri.dir/src/main.cpp.o"
+  "CMakeFiles/butsuri.dir/src/window.cpp.o"
+  "butsuri"
+  "butsuri.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/butsuri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
